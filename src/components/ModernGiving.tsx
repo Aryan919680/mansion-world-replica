bb40ca0f976d6237c2286f9738b049742b7c0d99
Aryan Malpotra <aryanmalpotra@gmail.com>
2025-08-08 17:28:57 +0530

    changes

--- src/components/ModernGiving.tsx
+++ src/components/ModernGiving.tsx
@@ -2,13 +2,13 @@ export const ModernGiving = () => {
   return (
     <section className="px-4 py-12 md:py-16 text-black">
       <div className="w-full max-w-[400px] md:w-[400px] mx-auto text-center">
-        <h2 className="text-[18px] md:text-[24px] font-heading mb-4 md:mb-6">Modern Giving</h2>
-        <p className="text-[13px] md:text-[18px] leading-relaxed">
+        <h2 className="lg:text-[18px] md:text-[24px] font-heading mb-4 md:mb-6">Modern Giving</h2>
+        <p className="lg:text-[13px] md:text-[18px] leading-relaxed">
         Our curated collection of luxury gifts are thoughtfully paired and pre packed to make it simple for you to send unique gifts to your loved ones.
 
 
         </p>
-     <p className="text-[13px] md:text-[18px] leading-relaxed mt-2">
+     <p className="lg:text-[13px] md:text-[18px] leading-relaxed mt-2">
         Join us on our mission to provide exceptional and luxurious gifts in India with unique personal touches. 
         </p>
 
