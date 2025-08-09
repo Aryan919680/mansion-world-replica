6f37d765c93a10197278d3216ad173f9644793a2
Aryan Malpotra <aryanmalpotra@gmail.com>
2025-08-09 22:22:17 +0530

    chnages

--- src/components/BrandStatement.tsx
+++ src/components/BrandStatement.tsx
@@ -2,9 +2,9 @@ export const BrandStatement = () => {
   return (
     <section className="px-4 py-12 md:py-16 text-black">
       <div className="w-full max-w-[400px] md:w-[400px] mx-auto text-center">
-        <h2 className="lg:text-[18px] md:text-[28px] font-heading mb-4 md:mb-6">Your Gifting Destination</h2>
+        <h2 className="lg:text-[18px] md:text-[28px] font-heading mb-4 md:mb-6">Your Marketing Destination</h2>
         <p className="lg:text-[13px] md:text-[22px] leading-relaxed">
-         At Opulent, a luxury gifting brand from India,
+         Opulent a luxury marketing brand from India,
           we believe that every thoughtful gift is a meaningful gesture — a bridge that strengthens both personal and corporate relationships. For us, gifting is about creating moments of surprise,
          delight, and lasting impression. It's not just a present, it's a statement of care, elegance, and intent. 
         </p>
