e66245b4fd54c020b4a5bfe26885b3a7ae0f7867
Aryan Malpotra <aryanmalpotra@gmail.com>
2025-08-09 18:25:13 +0530

    chnages

--- src/components/BrandStatement.tsx
+++ src/components/BrandStatement.tsx
@@ -2,8 +2,8 @@ export const BrandStatement = () => {
   return (
     <section className="px-4 py-12 md:py-16 text-black">
       <div className="w-full max-w-[400px] md:w-[400px] mx-auto text-center">
-        <h2 className="lg:text-[18px] md:text-[24px] font-heading mb-4 md:mb-6">Your Gifting Destination</h2>
-        <p className="lg:text-[13px] md:text-[18px] leading-relaxed">
+        <h2 className="lg:text-[18px] md:text-[28px] font-heading mb-4 md:mb-6">Your Gifting Destination</h2>
+        <p className="lg:text-[13px] md:text-[22px] leading-relaxed">
          At Opulent, a luxury gifting brand from India,
           we believe that every thoughtful gift is a meaningful gesture — a bridge that strengthens both personal and corporate relationships. For us, gifting is about creating moments of surprise,
          delight, and lasting impression. It's not just a present, it's a statement of care, elegance, and intent. 
--- src/components/ModernGiving.tsx
+++ src/components/ModernGiving.tsx
@@ -2,8 +2,8 @@ export const ModernGiving = () => {
   return (
     <section className="px-4 py-12 md:py-16 text-black">
       <div className="w-full max-w-[400px] md:w-[400px] mx-auto text-center">
-        <h2 className="lg:text-[18px] md:text-[24px] font-heading mb-4 md:mb-6">Modern Giving</h2>
-        <p className="lg:text-[13px] md:text-[18px] leading-relaxed">
+        <h2 className="lg:text-[18px] md:text-[28px] font-heading mb-4 md:mb-6">Modern Giving</h2>
+        <p className="lg:text-[13px] md:text-[22px] leading-relaxed">
         Our curated collection of luxury gifts are thoughtfully paired and pre packed to make it simple for you to send unique gifts to your loved ones.
 
 
