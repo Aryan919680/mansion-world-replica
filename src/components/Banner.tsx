ebf69e2cb6330c85f961241359ce9607d3403477
Aryan Malpotra <140583692+Aryan919680@users.noreply.github.com>
2026-03-27 06:54:23 +0000

    fixes

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,8 +8,8 @@ export const Banner = () => {
         style={{ backgroundImage: `url(${image})` }}
       >
         <div className="absolute inset-0 flex items-center justify-center">
-          <div className="lg:h-[300px] lg:w-[200px] md:w-[300px] md:h-[300px] bg-[#dcd7d2] text-black p-4 rounded shadow-lg text-center flex items-center justify-center">
-            <p className="lg:text-[13px] md:text-[22px] leading-relaxed">
+          <div className="lg:h-[300px] lg:w-[200px] sm:w-[300px] sm:h-[300px] bg-[#dcd7d2] text-black p-4 rounded shadow-lg text-center flex items-center justify-center">
+            <p className="lg:text-[13px] sm:text-[25px] leading-relaxed">
               Welcome to Opulent — where luxury meets purpose. Discover premium gifts that make lasting impressions.
             </p>
           </div>
--- src/components/ModernGiving.tsx
+++ src/components/ModernGiving.tsx
@@ -1,14 +1,14 @@
 export const ModernGiving = () => {
   return (
-    <section className="px-4 py-12 md:py-16 text-black">
+    <section className="px-4 py-12 sm:py-20 text-black">
       <div className="w-full max-w-[400px] md:w-[400px] mx-auto text-center">
-        <h2 className="lg:text-[18px] md:text-[28px] font-heading mb-4 md:mb-6">Modern Giving</h2>
-        <p className="lg:text-[13px] md:text-[22px] leading-relaxed">
+        <h2 className="lg:text-[18px] sm:text-[30px] font-heading mb-4 md:mb-6">Modern Giving</h2>
+        <p className="lg:text-[13px] sm:text-[25px] leading-relaxed">
         Our curated collection of luxury gifts are thoughtfully paired and pre packed to make it simple for you to send unique gifts to your loved ones.
 
 
         </p>
-     <p className="lg:text-[13px] md:text-[22px] leading-relaxed mt-2">
+     <p className="lg:text-[13px] sm:text-[25px] leading-relaxed mt-2">
         Join us on our mission to provide exceptional and luxurious gifts in India with unique personal touches. 
         </p>
 
